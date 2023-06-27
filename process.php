a601eed38e4290e9a82949c2601c0280c6e6d9a4
Hezis <radware>
2023-06-27 23:43:16 +0300

    added files and some logic

--- process.php
+++ process.php
@@ -1,17 +1,32 @@
 <!-- process.php -->
 <?php
+//die("reached");
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $url = $_POST['url'];
-    $data = $_POST['data'];
+    $headers_data = $_POST['headers'];
+    $headers = explode("\r\n", $headers_data);
 
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    $curl = curl_init($url);
+    //curl_setopt($curl, CURLOPT_FAILONERROR, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HEADER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    if(array_key_exists("sslchk", $_POST) && $_POST['sslchk'] === "on"){
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    }
 
-    $response = curl_exec($ch);
-    curl_close($ch);
+    curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+
+    $response = curl_exec($curl);
+    if(!$response){
+        $response = curl_error($curl);
+    }
+    curl_close($curl);
 
     // Display the response
+
     echo "<pre>" . htmlentities($response) . "</pre>";
 }
 ?>
